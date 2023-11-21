a3b3be84e82b48d692474eef7b4d3075012aed65
Christian <christian.a.faber@gmail.com>
2023-11-20 22:14:02 -1000

    set it up better

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./input.css";
 import "./normalize.css";
-import "./dist/output.css";
+import "./output.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import portfolio from "../data/portfolio.json";
 
 export const Portfolio = () => {
   return (
     <div>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+      {portfolio.portfolio.map((item) => (
+        <div>
+          <p>{item.title}</p>
+        </div>
+      ))}
     </div>
   );
 };
