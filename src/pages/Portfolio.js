350d2ebefe0fa11881199ba81884954a8fe5ccb3
Christian <christian.a.faber@gmail.com>
2023-12-12 19:45:43 -1000

    add links to portfolio page:

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,15 @@
 import portfolio from "../data/portfolio.json";
 import { PortfolioTile } from "../components/PortfolioTile";
+import { Link } from "react-router-dom";
 
 export const Portfolio = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/experience">experience</Link>
+        <Link to="/">Home</Link>
+      </div>
       {portfolio.portfolio.map((item) => (
         <PortfolioTile item={item} />
       ))}
